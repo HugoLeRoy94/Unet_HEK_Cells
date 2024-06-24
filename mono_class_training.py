adaef2af30f06624ee8da6fafab7587ad83a0469
cachetdaspirine <leroyhugo@yahoo.fr>
2024-06-24 14:59:31 +0200

    test another architecture

--- mono_class_training.py
+++ mono_class_training.py
@@ -2,63 +2,152 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-def unet_model(input_size=(256, 256, 1)):
-    inputs = tf.keras.Input(input_size)
+#def unet_model(input_size=(256, 256, 1)):
+#    inputs = tf.keras.Input(input_size)
+#
+#    # Encoder
+#    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
+#    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv1)
+#    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+#
+#    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool1)
+#    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv2)
+#    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+#
+#    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(pool2)
+#    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv3)
+#    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+#
+#    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(pool3)
+#    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv4)
+#    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(conv4)
+#
+#    # Bottleneck
+#    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+#    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+#
+#    # Decoder
+#    up6 = layers.Conv2D(512, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv5))
+#    merge6 = layers.concatenate([conv4, up6], axis=3)
+#    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(merge6)
+#    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv6)
+#
+#    up7 = layers.Conv2D(256, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv6))
+#    merge7 = layers.concatenate([conv3, up7], axis=3)
+#    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(merge7)
+#    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv7)
+#
+#    up8 = layers.Conv2D(128, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv7))
+#    merge8 = layers.concatenate([conv2, up8], axis=3)
+#    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(merge8)
+#    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv8)
+#
+#    up9 = layers.Conv2D(64, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv8))
+#    merge9 = layers.concatenate([conv1, up9], axis=3)
+#    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(merge9)
+#    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv9)
+#    conv9 = layers.Conv2D(2, 3, activation='relu', padding='same')(conv9)
+#
+#    outputs = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
+#
+#    model = models.Model(inputs=[inputs], outputs=[outputs])
+#
+#    #model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])    
+#    
+#    return model
 
-    # Encoder
-    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
-    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv1)
-    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+# https://youtu.be/jvZm8REF2KY
+"""
+Standard Unet
+Model not compiled here, instead will be done externally to make it
+easy to test various loss functions and optimizers. 
+"""
 
-    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool1)
-    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv2)
-    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(pool2)
-    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv3)
-    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+from keras import backend as K
 
-    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(pool3)
-    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv4)
-    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(conv4)
+def jacard_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
-    # Bottleneck
-    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(pool4)
-    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(conv5)
 
-    # Decoder
-    up6 = layers.Conv2D(512, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv5))
-    merge6 = layers.concatenate([conv4, up6], axis=3)
-    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(merge6)
-    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv6)
 
-    up7 = layers.Conv2D(256, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv6))
-    merge7 = layers.concatenate([conv3, up7], axis=3)
-    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(merge7)
-    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv7)
+################################################################
+def multi_unet_model(n_classes=1, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
 
-    up8 = layers.Conv2D(128, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv7))
-    merge8 = layers.concatenate([conv2, up8], axis=3)
-    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(merge8)
-    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv8)
-
-    up9 = layers.Conv2D(64, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv8))
-    merge9 = layers.concatenate([conv1, up9], axis=3)
-    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(merge9)
-    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv9)
-    conv9 = layers.Conv2D(2, 3, activation='relu', padding='same')(conv9)
-
-    outputs = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
-
-    model = models.Model(inputs=[inputs], outputs=[outputs])
-
-    #model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])    
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.2)(c1)  # Original 0.1
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.2)(c2)  # Original 0.1
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.2)(c8)  # Original 0.1
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.2)(c9)  # Original 0.1
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    #model.summary()
     
     return model
+ 
 
 # Create the model
-model = unet_model()
+model = multi_unet_model()
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
@@ -138,4 +227,4 @@ val_mask_generator = mask_datagen.flow(val_masks, batch_size=batch_size, seed=se
 val_generator = custom_generator(val_image_generator, val_mask_generator)
 # Fit the model
 history = model.fit(train_generator, steps_per_epoch=len(train_images) // batch_size, epochs=10, validation_data=val_generator, validation_steps=len(val_images) // batch_size)
-model.save('UNET_mono.h5')
+model.save('UNET_mono_b.h5')
